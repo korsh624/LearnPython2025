977b35fb163423ecd157af219e184e468547bf1c
korsh624 <korsh624@gmail.com>
2025-11-20 12:47:19 +0300

    add nrw learn Dima

--- Dima/17/task1.py
+++ Dima/17/task1.py
@@ -6,7 +6,7 @@
 '''
 count =0
 maxnum=0
-for i in range(1012, 9639):
+for i in range(1012, 9638+1):
     if i%3==0 and i%11 != 0 and i%13 != 0 and i%17 != 0 and i%19 != 0:
         count += 1
         maxnum = i
