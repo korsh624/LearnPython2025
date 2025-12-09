77f96a514a64ef9bdef24c50a28cf0520b51d6a2
korsh624 <korsh624@gmail.com>
2025-12-09 15:45:30 +0300

    add main ang eval example

---
+++ Dima/17/task85.py
@@ -0,0 +1,22 @@
+
+count = 0
+max_num = 0
+
+for i in range(1111, 10000):
+    digits = [int(d) for d in str(i)]
+    s = sum(digits)
+    p = 1
+    for d in digits:
+        p *= d
+
+    try:
+        # Проверяем делимость на сумму и произведение цифр
+        if i % s == 0 and i % p == 0:
+            count += 1
+            if i > max_num:
+                max_num = i
+    except ZeroDivisionError:
+        # Если p == 0 (произведение цифр нулевое), пропускаем число
+        continue
+
+print(count, max_num)
