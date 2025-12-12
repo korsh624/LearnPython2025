eb335569cc036fb4a0017ccd2a8f25130975ed63
korsh624 <korsh624@gmail.com>
2025-12-12 12:27:27 +0300

    add dz 12122025

---
+++ Dima/17/65.py
@@ -0,0 +1,8 @@
+# s='У лкуоморья дуб зеленый'
+a=465874365
+def fiidMult(a):
+    s=str(a)
+    mutl=1
+    for char in s:
+        mutl=int(char)*mutl
+    return mutl
--- Timur/forwhile.py
+++ Timur/forwhile.py
@@ -2,18 +2,37 @@
 # while cmd!='stop':
 #     cmd=input('Enter command: ')
 #     print(cmd)
-
-
 # r=range(10)
 # r=range(10,30)
 # r=range(10,30,5)
 # print(*r)
-word='Vladimir'
-
-for char in word:
-    print(char)
-
-for i in range(10):
-    print('Hello World')
-
-
+# word='Vladimir'
+#
+# for char in word:
+#print(char)
+# import cv2
+# img=cv2.imread('../i.jpeg')
+# print(type(img))
+# for i in range(10):
+#     print('Hello World')
+#     print(i)
+#
+# for i in range(1280):
+#     for j in range(720):
+#         for k in range(3):
+#             print(img[i,j,k])
+l=[1,2,3,7,3,5,3,4]
+print(l.append(12))
+print(l)
+print(l.count(3))
+l.clear()
+print(l)
+t=(1,2,3,-7,3,-5,-3,4)
+print(t)
+i=t.index(3)
+print(i)
+valid=[]
+for item in t:
+    if item <0:
+        valid.append(item)
+print(tuple(valid))
